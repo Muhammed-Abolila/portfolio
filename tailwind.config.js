79c3d5a68258cc18edd580dff021c78ad6c3289e
Muhammed Abo Lila <mabolila638@gmail.com>
2025-05-08 01:42:17 +0300

    feat(bottom navbar): add links-animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",              
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
-      colors:{
-        firstBgColor:"var(--firstBgColor)",
-        secondBgColor:"var(--secondBgColor)",
-        identityColor:"var(--identityColor)",
-        mainColor:"var(--mainColor)",
-        grayColor:"var(--grayColor)",
-      }
+      keyframes: {
+        "links-animation": {
+          "0%": { transform: "translateY(7px)" },
+          "100%": { transform: "translateY(3px)" },
+        },
+      },
+      animation: {
+        "links-animation": "links-animation 0.2s alternate 4",
+      },
+      colors: {
+        firstBgColor: "var(--firstBgColor)",
+        secondBgColor: "var(--secondBgColor)",
+        identityColor: "var(--identityColor)",
+        mainColor: "var(--mainColor)",
+        grayColor: "var(--grayColor)",
+      },
     },
   },
   plugins: [],
-}
+};
